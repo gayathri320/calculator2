954ffd4883ab50ff5ed6a0068f5f3dba0a1b8a75
Gayathri A <gayathria932002@gmail.com>
2026-04-02 10:04:16 +0530

    this is my calculator with subtraction

--- calculator2.py
+++ calculator2.py
@@ -1,12 +1,21 @@
 import logging
 logging.basicConfig(level=logging.INFO,format='%(levelname)s-%(message)s')
-def add(a, b):
+def add(a,b):
     try:
         result=a+b
         logging.info(f"The sum is {result}")
         return result
     except Exception as e:
-        logging.error(f"Error in addition: {e}")
+        logging.error(f"Error:{e}")
+        return None
+
+def subtraction(a,b):
+    try:
+        result1=a-b
+        logging.info(f"The sum is {result1}")
+        return result
+    except Exception as e:
+        logging.error(f"Error:{e}")
         return None
 
 
