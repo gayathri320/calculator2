3fda6001992449d0ee5b64d0638e6dda70baa7b4
Gayathri A <gayathria932002@gmail.com>
2026-04-02 10:11:09 +0530

    this is my calculator to which division is added

--- calculator2.py
+++ calculator2.py
@@ -21,9 +21,17 @@ def subtraction(a,b):
 def multiplication(a,b):
     try:
         result2=a*b
-        logging.info(f"The difference is {result2}")
+        logging.info(f"The product is {result2}")
         return result2
     except Exception as e:
         logging.error(f"Error:{e}")
         return None
 
+def division(a,b):
+    try:
+        result3=a/b
+        logging.info(f"The quotient is {result3}")
+        return result3
+    except Exception as e:
+        logging.error(f"Error:{e}")
+        return None
